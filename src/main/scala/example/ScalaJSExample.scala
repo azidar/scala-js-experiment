ef1bee13d6d984800d0d83e2f72451bdca2cfe4a
azidar <azidar@gmail.com>
2014-09-28 09:05:31 -0700

    Cleaned up code, no new functionality

--- src/main/scala/example/ScalaJSExample.scala
+++ src/main/scala/example/ScalaJSExample.scala
@@ -12,8 +12,8 @@ case class Point(x: Double, y: Double){
   def length = Math.sqrt(x * x + y * y)
 }
 
-case class Ball(var pos: Point, var vel: Point, val w:Int, val h:Int)
-case class Paddle(var pos: Point, var vel:Point, val w:Int, val h:Int)
+case class Ball(var pos: Point, var vel: Point, val w:Int=20, val h:Int=20)
+case class Paddle(var pos: Point, var vel:Point=Point(0,0), val w:Int=20, val h:Int=200)
 
 @JSExport
 object ScalaJSExample {
@@ -25,14 +25,15 @@ object ScalaJSExample {
                   .asInstanceOf[dom.HTMLCanvasElement]
   val ctx = canvas.getContext("2d")
                   .asInstanceOf[dom.CanvasRenderingContext2D]
-  var player  = Paddle(Point(dom.innerWidth.toInt*3/4, dom.innerHeight.toInt/2),Point(0,0),20,200)
-  var player2 = Paddle(Point(dom.innerWidth.toInt/4, dom.innerHeight.toInt/2),Point(0,0),20,200)
+  var player1 = Paddle(Point(dom.innerWidth.toInt*3/4, dom.innerHeight.toInt/2))
+  var player2 = Paddle(Point(dom.innerWidth.toInt/4, dom.innerHeight.toInt/2))
 
-  var ball = Ball(Point(dom.innerWidth/2, dom.innerHeight.toInt/2),Point(4,0),20,20)
+  var ball = Ball(Point(dom.innerWidth/2, dom.innerHeight.toInt/2),Point(4,0))
 
   var death: Option[(String, Int)] = None
   def collision(a:Paddle, b:Ball):Boolean = {
-    if((Math.abs(a.pos.x - b.pos.x) <= ( a.w/2 + b.w/2)) && (b.pos.y > (a.pos.y - a.h/2) ) && (b.pos.y < (a.pos.y + a.h/2))) {
+    val nb = Ball(ball.pos + ball.vel,ball.vel)
+    if((Math.abs(a.pos.x - nb.pos.x) <= ( a.w/2 + nb.w/2)) && (nb.pos.y > (a.pos.y - a.h/2) ) && (nb.pos.y < (a.pos.y + a.h/2))) {
       true 
     } else false
   }
@@ -42,95 +43,40 @@ object ScalaJSExample {
     canvas.width = dom.innerWidth
 
     // doing
-    if(ball.pos.x <= 0) death = Some(("Player 2 Wins!",100))
-    if(ball.pos.x >= dom.innerWidth.toInt) death = Some(("Player 1 Wins!",100))
+    if(ball.pos.x <= 0) death = Some(("Player 1 Wins!",100))
+    if(ball.pos.x >= dom.innerWidth.toInt) death = Some(("Player 2 Wins!",100))
     if(ball.pos.y - ball.w/2 <= 0) ball.vel = Point(ball.vel.x,-1*ball.vel.y)
     if(ball.pos.y + ball.w/2 >= dom.innerHeight) ball.vel = Point(ball.vel.x,-1*ball.vel.y)
 
-    val nextBall = Ball(ball.pos + ball.vel,ball.vel,ball.w,ball.h)
-    if(collision(player,nextBall)) {
-      ball.vel = Point(-1*ball.vel.x, ball.vel.y + 0.2 * player.vel.y)
-      ball.pos = ball.pos + player.vel
-    } else if(collision(player2,nextBall)) {
+    if(collision(player1,ball)) {
+      ball.vel = Point(-1*ball.vel.x, ball.vel.y + 0.2 * player1.vel.y)
+      ball.pos = ball.pos + player1.vel
+    } else if(collision(player2,ball)) {
       ball.vel = Point(-1*ball.vel.x, ball.vel.y + 0.2 * player2.vel.y)
       ball.pos = ball.pos + player2.vel
     } else {
       ball.pos = ball.pos + ball.vel
     }
+
     //update player based on keyboard input
-    
+    def updatePlayer(p:Paddle,keys:List[Int]):Unit = {
+      val deltas = List(Point(0,-6),Point(-6,0),Point(6,0),Point(0,6))
+      p.vel = Point(0, 0)
+      keys.zipWithIndex.map( {case (x,i) => {
+        if (keysDown(x)) {
+          p.pos += deltas(i)
+          p.vel = deltas(i)
+        }}})
+    }
+
     //player1 uses arrow keys
-    player.vel = Point(0, 0)
-    if (keysDown(38)) {
-      player.pos += Point(0, -6)//up
-      player.vel = Point(0, -6)
-    }
-    if (keysDown(37)) {
-      player.pos += Point(-6, 0)//let
-      player.vel = Point(-6,0)
-    }
-    if (keysDown(39)) {
-      player.pos += Point(6, 0)//right
-      player.vel = Point(6, 0)
-    }
-    if (keysDown(40)) {
-      player.pos += Point(0, 6)//down
-      player.vel = Point(0, 6)
-    }
-
+    val p1keys = List(38,37,39,40)
+    updatePlayer(player1,p1keys)
     //player2 uses WASD keys
-    /*
-    if (keysDown(87)) player2.pos += Point(0, -2)//up(W)
-    if (keysDown(65)) player2.pos += Point(-2, 0)//left(A)
-    if (keysDown(68)) player2.pos += Point(2, 0)//right(D)
-    if (keysDown(83)) player2.pos += Point(0, 2)//down(S)
-    */
-    player2.vel = Point(0, 0)
-    if (keysDown(87)) {
-      player2.pos += Point(0, -6)//up
-      player2.vel = Point(0, -6)
-    }
-    if (keysDown(65)) {
-      player2.pos += Point(-6, 0)//let
-      player2.vel = Point(-6,0)
-    }
-    if (keysDown(68)) {
-      player2.pos += Point(6, 0)//right
-      player2.vel = Point(6, 0)
-    }
-    if (keysDown(83)) {
-      player2.pos += Point(0, 6)//down
-      player2.vel = Point(0, 7)
-    }
+    val p2keys = List(87,65,68,83)
+    updatePlayer(player2,p2keys)
   }
 
-/*
-    balls = balls.filter(e =>
-      e.pos.x >= 0 && e.pos.x <= canvas.width &&
-      e.pos.y >= 0 && e.pos.y <= canvas.height
-    )
-
-    //def randSpeed = Random.nextInt(5) - 3
-    balls = balls ++ Seq.fill(20 - balls.length)(
-      new Ball(
-        Point(Random.nextInt(canvas.width.toInt), 0),
-        Point(randSpeed, randSpeed)
-      )
-    )
-
-    for(ball <- balls){
-      ball.pos = ball.pos + ball.vel
-      val delta = player - ball.pos
-      ball.vel = ball.vel + delta / delta.length / 100
-    }
-
-    if(balls.exists(e => (e.pos - player).length < 20)){
-      death = Some((s"You lasted $deltaT seconds", 100))
-      balls = balls.filter(e => (e.pos - player).length > 20)
-    }
-    
-  }
-  */
   def deltaT = ((js.Date.now() - startTime) / 1000).toInt
 
   def draw() = {
@@ -142,8 +88,8 @@ object ScalaJSExample {
       case None =>
 
         ctx.fillStyle = "white"
-        ctx.fillRect(player.pos.x - 10, player.pos.y - 100, 20, 200)
-        ctx.fillText("player1", player.pos.x - 15, player.pos.y - 30)
+        ctx.fillRect(player1.pos.x - 10, player1.pos.y - 100, 20, 200)
+        ctx.fillText("player1", player1.pos.x - 15, player1.pos.y - 30)
 
         ctx.fillStyle = "green"
         ctx.fillRect(player2.pos.x - 10, player2.pos.y - 100, 20, 200)
@@ -172,8 +118,7 @@ object ScalaJSExample {
   @JSExport
   def main(): Unit = {
     dom.console.log("main")
-
-/*
+    /*
     dom.document.onmousemove = { (e: dom.MouseEvent) =>
       player = Paddle(Point(e.clientX.toInt, e.clientY.toInt),Point(0,0),20,200)
       (): js.Any
